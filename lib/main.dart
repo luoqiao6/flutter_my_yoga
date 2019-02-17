ca07cbf16972967245784a7274101da77b289d87
luoqiao <luoqiaowork@qq.com>
2019-02-17 14:54:18 +0800

    修改入口文件

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_my_yoga/pages/init_page.dart';
 import 'package:flutter_my_yoga/pages/login_page.dart';
+import 'package:flutter_my_yoga/utils/colors.dart';
+import 'package:flutter_my_yoga/utils/textStyles.dart';
 
-
-Color red = Color.fromARGB(255, 254, 77, 151);
-Color yellow = Color.fromARGB(255, 255, 216, 20);
-Color purple = Color.fromARGB(255, 48, 18, 88);
-Color blue = Color.fromARGB(255, 43, 68, 255);
-Color green = Color.fromARGB(255, 0, 217, 101);
-Color black = Color.fromARGB(255, 77, 77, 77);
-Color black50 = Color.fromARGB(125, 77, 77, 77);
-Color white = Color.fromARGB(255, 255, 246, 246);
 
 //    Google Sans-Bold
 //    Google Sans-Medium
@@ -20,7 +13,6 @@ Color white = Color.fromARGB(255, 255, 246, 246);
 //    Montserrat-Regular
 //    Montserrat-SemiBold
 //    Telegrafico-Regular
-
 
 
 void main() => runApp(MyYoga());
@@ -42,16 +34,16 @@ class MyYogaState extends State<MyYoga> {
     return MaterialApp(
       title: 'My Yoga',
       theme: ThemeData(
-        primaryColor: red,
-        backgroundColor: red,
-        accentColor: red,
-        scaffoldBackgroundColor: red,
+        primaryColor: YogaColors.red,
+        backgroundColor: YogaColors.red,
+        accentColor: YogaColors.red,
+        scaffoldBackgroundColor: YogaColors.red,
         textTheme: TextTheme(
           body1: TextStyle(
-            color: Colors.white,
+            color: YogaColors.white,
             fontSize: 30,
           ),
-          body2: TextStyle(color: black),
+          body2: YogaTextStyles.googleSanRed,
 
         )
       ),
